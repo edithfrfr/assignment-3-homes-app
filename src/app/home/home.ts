107da37de3fd06ba081fae5006a0136708d1bbf4
edithfrfr <edithhernandez03289@gmail.com>
2026-02-25 19:31:59 -0600

    Added HousingLocationInfo Interface and created a single instance of the new interface

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -8,5 +8,15 @@ import { HousingLocation } from '../housing-location/housing-location';
   styleUrl: './home.css',
 })
 export class Home {
-
+readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+housingLocation: HousingLocationInfo = {
+id: 9999,
+name: 'Test Home',
+city: 'Test city',
+state: 'ST',
+photo: `${this.baseUrl}/example-house.jpg`,
+availableUnits: 99,
+wifi: true,
+laundry: false,
+};
 }
