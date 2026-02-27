a4b588743888b6a004bcbd8858f364c069c4379c
edithfrfr <edithhernandez03289@gmail.com>
2026-02-27 14:42:46 -0600

    Added Inputs to the HousingLocation Component

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-housing-location',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './housing-location.css',
 })
 export class HousingLocation {
-
+housingLocation = input.required
 }
